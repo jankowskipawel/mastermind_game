264c5a7d899c64ead8170a51fdf7a5ac7d3122b5
Pawel MacOS <paweljankowski246@gmail.com>
2019-03-23 22:56:25 +0100

    Fixed a lot of bugs

--- mastermind.rb
+++ mastermind.rb
@@ -41,11 +41,16 @@ attr_accessor :code
     puts "Type in your guess separated by spaces (e.g. 1 3 5 4): "
     input = gets.chomp.split
     result = []
+    if input.empty?
+      puts "Incorrect input. Try again"
+      resulto = Game.get_player_choice()
+      return resulto
+    end
     input.each do |x|
       if !x.to_i.between?(1,9) || input.length != 4
         puts "Incorrect input. Try again"
-        Game.get_player_choice()
-        return
+        resulto = Game.get_player_choice()
+        return resulto
       else
         result << x.to_i
       end
@@ -53,31 +58,72 @@ attr_accessor :code
     return result
   end
 
-  def check_choice(code_array, choice_array)
+  def check_reds(code_array, choice_array)
+    result = []
+    code_array.each_with_index do |code_val, code_index|
+      is_color = false
+      choice_array.each_with_index do |choice_val, choice_index|
+        if code_val == choice_val && code_index == choice_index
+          result << "r".red
+          is_color = true
+          next
+        end
+      end
+        is_color == false ? result << "-" : true
+    end
+    return result
+  end
+  
+  def check_whites(code_array, choice_array)
     result = []
     choice_array.each_with_index do |choice_val, choice_index|
       is_color = false
       code_array.each_with_index do |code_val, code_index|
         if code_val == choice_val
-          if code_index == choice_index
-            result << "r".red
-            is_color = true
-            next
-          else
-            result << "w"
-            is_color = true
-            next
-          end
+          result << "w"
+          is_color = true
+          break
         end
       end
-      is_color == false ? result << "-" : true
+        is_color == false ? result << "-" : true
     end
     return result
   end
 
+  def check_choice(code_array, choice_array)
+    result = []
+    reds = self.check_reds(code_array, choice_array)
+    whites = self.check_whites(code_array, choice_array)
+    result = []
+    reds.each_with_index do |x, ind|
+      if x == "\e[31mr\e[0m"
+        result[ind] = "r".red
+      else
+        result[ind] = "-"
+      end
+    end
+    whites.each_with_index do |x, ind|
+      if result[ind] == "-" && x == "w"
+        result[ind] = "w"
+      end
+    end
+    return result
+  end
+
+  def is_won?(colors_arr)
+    colors_arr.each do |col|
+      if col == "\e[31mr\e[0m"
+        next
+      else
+        return false
+      end
+    return true
+    end
+  end
+
   def game_loop()
     puts "                 " + "MASTERMIND GAME".bg_magenta
-    puts "     " + "Guess what code I am thinking of. Digits (1-9)".magenta
+    puts "     " + "Guess what 4 digit code I am thinking of. Digits (1-9)".magenta
     puts "w" + " - digit is in code but not in this spot | ".magenta + "r".red + " - digit is in this spot".magenta
 
     round = 0
@@ -89,6 +135,13 @@ attr_accessor :code
       choice = Game.get_player_choice()
       colors = check_choice(self.code, choice)
       puts "#{colors[0]} #{colors[1]} #{colors[2]} #{colors[3]}"
+      self.is_won?(colors) ? round = max_rounds : true
+      if self.is_won?(colors)
+        puts "YOU WON!".bg_green
+        break
+      elsif !self.is_won?(colors) && round == max_rounds-1
+        puts "YOU LOST!".bg_red
+      end
       round += 1
     end
   end
@@ -96,8 +149,4 @@ end
 
 g1 = Game.new([1,2,3,4])
 
-# puts g1.code
-# puts "\n"
-# g1.randomize
-# puts g1.code
 g1.game_loop()
